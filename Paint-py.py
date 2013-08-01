8fc39c7f66d7a9bed826b6f01b4af9f5e1fc23cd
kmbrlynn <douglas.kimberly.a@gmail.com>
2013-08-01 14:27:14 -0400

    heavily comments and blue button added

--- Paint-py.py
+++ Paint-py.py
@@ -7,12 +7,17 @@
 
 from Tkinter import *
 
+# random var for testing
+
+foo = 3
+
+
 # this will hold a pressed or released value
 mouseState = "up"
 
-# null value will pass thru these on release, making the draw not happen
-xOnRelease = None       
-yOnRelease = None
+# this will hold the x or y value of the cursor. null if not pressed
+xState = None       
+yState = None
 
 def main():
     root = Tk()
@@ -23,40 +28,58 @@ def main():
     screen_height = root.winfo_screenheight()
     # set the window size
     root.minsize(screen_width/2, screen_height/2)
+    
     # create a canvas
     myCanvas = Canvas(root)
     # fit the canvas to the window. width and height are built into Tkinter
     myCanvas.config(width = screen_width/2, height = screen_height/2)
     
+    # create a button for blue
+    blueButton = Button(root, text ="Blue", command = bluePressed)
+    
     # this packs widgets into rows and columns
     myCanvas.pack()
+    blueButton.pack()
+    
     # bind my functions to built-in Tkinter mouse events
     myCanvas.bind("<Motion>", moveMouse)
     myCanvas.bind("<ButtonPress-1>", press)
     myCanvas.bind("<ButtonRelease-1>", release)
     root.mainloop()
 
+# function for when mouse is in pressed position
 def press(event):
     # don't forget to include global vars in your function
-    global mouseState       
-    mouseState = "down"           
+    global mouseState
+    mouseState = "down"
 
+# function for when you move the mouse
+def moveMouse(event):
+    # essentially, if press() is true...
+    if mouseState == "down":
+        global xState
+        global yState
+        # ...and xState and yState are null - aka release() is NOT true, 
+        if xState is not None and yState is not None:
+            event.widget.create_line(xState,yState,event.x,event.y,smooth=TRUE)
+        # then populate xState and yState with the coords of the mouse position - aka draw :)
+        xState = event.x
+        yState = event.y
+
+# function for when mouse is in released position
 def release(event):
-    global mouseState, xOnRelease, yOnRelease       
+    global mouseState
+    global xState
+    global yState
     mouseState = "up"
     # reset the line when you let go of the button
-    xOnRelease = None           
-    yOnRelease = None
+    xState = None           
+    yState = None
 
-def moveMouse(event):
-    if mouseState == "down":
-        global xOnRelease, yOnRelease
-        # detect whether or not the mouse is pressed
-        if xOnRelease is not None and yOnRelease is not None:
-            event.widget.create_line(xOnRelease,yOnRelease,event.x,event.y,smooth=TRUE)
-        # draw it! :)
-        xOnRelease = event.x
-        yOnRelease = event.y
+# function for when you press the blue button
+def bluePressed():
+    foo = 5
+    print("blue clicked")
 
 if __name__ == "__main__":
     main()
